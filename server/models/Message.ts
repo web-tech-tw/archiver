ab6683ac81b2e4a9070a6f695f81de08bd50cff6
不知火 <supersonic@livemail.tw>
2026-07-22 18:11:22 +0800

    feat: add MessageDocument interface and modify schema for string _id

--- server/models/Message.ts
+++ server/models/Message.ts
@@ -1,17 +1,22 @@
 import {Schema, model, models} from 'mongoose';
 import type {Message} from '../utils/parser';
 
-const messageSchema = new Schema<Message>(
+export interface MessageDocument extends Omit<Message, 'hash'> {
+  _id: string;
+}
+
+const messageSchema = new Schema<MessageDocument>(
     {
+      _id: {type: String, required: true},
       date: {type: String, required: true},
       time: {type: String, required: true},
       content: {type: String, required: true},
-      hash: {type: String, required: true, unique: true, index: true},
     },
     {
       timestamps: true,
+      _id: false,
     },
 );
 
 export const MessageModel =
-    models.Message || model<Message>('Message', messageSchema);
+    models.Message || model<MessageDocument>('Message', messageSchema);
